6c8393996b63b1455b7ecff6d8e56e4fcdc1e4fe
Simone Gasparini <simone.gasparini.2@gmail.com>
2025-09-25 15:50:38 +0200

    Add trim_quotes function for export cmd

--- src/export.c
+++ src/export.c
@@ -6,7 +6,7 @@
 /*   By: sgaspari <sgaspari@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 12:48:38 by sgaspari          #+#    #+#             */
-/*   Updated: 2025/09/25 13:35:15 by sgaspari         ###   ########.fr       */
+/*   Updated: 2025/09/25 15:50:12 by sgaspari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,10 @@
 #include "libft.h"
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
+static char	*trim_all_quotes(char *arg);
 static bool	is_parameter_name_invalid(char *s);
 static bool	is_var_dup(char *s, t_node *list);
 static int	find_equal(char *s);
@@ -34,6 +36,7 @@ void	ft_export(t_cmd *p)
 	i = 1;
 	while (p->argv[i] != NULL)
 	{
+		p->argv[i] = trim_all_quotes(p->argv[i]);
 		if (is_parameter_name_invalid(p->argv[i]) == true)
 		{
 			ft_fprintf(STDERR_FILENO,
@@ -67,6 +70,35 @@ static bool	is_parameter_name_invalid(char *s)
 		i++;
 	}
 	return (false);
+}
+
+static char	*trim_all_quotes(char *arg)
+{
+	char	*new_str;
+	size_t	i;
+	size_t	c;
+
+	i = 0;
+	c = 0;
+	while (i < ft_strlen(arg))
+	{
+		if (arg[i] == '\'' || arg[i] == '"')
+			c++;
+		i++;
+	}
+	new_str = ft_calloc(ft_strlen(arg) - c + 1, 1);
+	if (new_str == NULL)
+		return (NULL);
+	i = 0;
+	c = 0;
+	while (i < ft_strlen(arg))
+	{
+		if (arg[i] != '\'' && arg[i] != '"')
+			new_str[c++] = arg[i];
+		i++;
+	}
+	free(arg);
+	return (new_str);
 }
 
 static bool	is_var_dup(char *s, t_node *list)
